530f5b60e11d1cf0ae92b740327261452e737939
nilabr <grunau@gmx.de>
2019-12-09 15:46:07 +0100

    CartId working

--- SQL/StoredProcedures.sql
+++ SQL/StoredProcedures.sql
@@ -12,14 +12,14 @@ GO
 
 /* GetProductDetails & Popularity +1 */
 CREATE OR ALTER PROCEDURE GetProductDetails
-(@ProcuctId int)
+    (@ProcuctId int)
 AS
 SELECT c.Name Category, p.Name Product, Price, InStock, Popularity
 FROM Products p
     INNER JOIN Categories c
     ON p.CategoryId = c.Id
-	WHERE p.Id = @ProcuctId
-	UPDATE Products 
+WHERE p.Id = @ProcuctId
+UPDATE Products 
 	SET Products.Popularity +=1
 	WHERE Products.Id = @ProcuctId
 GO
@@ -27,26 +27,28 @@ GO
 
 /* ListProductsByCategory */
 CREATE OR ALTER PROCEDURE ListProductsByCategory
-(@InStock int)
+    (@InStock int)
 AS
-	SELECT c.Name AS Kategori, p.Name Produkt, p.Price Pris, p.Popularity Popularitet
-	FROM Products p
-	INNER JOIN Categories c ON p.CategoryId = c.Id
-    
-	WHERE p.InStock = @InStock
-	OR p.InStock = 1
-	
-	GROUP BY c.Name, p.Name, p.Price, p.Popularity
-	ORDER BY c.Name, p.Popularity DESC
+SELECT c.Name AS Kategori, p.Name Produkt, p.Price Pris, p.Popularity Popularitet
+FROM Products p
+    INNER JOIN Categories c ON p.CategoryId = c.Id
+
+WHERE p.InStock = @InStock
+    OR p.InStock = 1
+
+GROUP BY c.Name, p.Name, p.Price, p.Popularity
+ORDER BY c.Name, p.Popularity DESC
 GO
 
 EXEC ListProductsByCategory 0
 GO
 
-/* CreateCart */
+/* CreateCart & Return CartId */
 CREATE OR ALTER PROCEDURE CreateCart
-    --@CartId int OUTPUT
-	(@ProductId int, @Amount int, @Price int)
+    @ProductId int,
+    @Amount int,
+    @Price int,
+    @CartId int output
 AS
 BEGIN
     INSERT INTO Cart
@@ -54,14 +56,23 @@ BEGIN
     VALUES
         (@ProductId, @Amount, @Price)
 
-    --SET @CartId = CAST(SCOPE_IDENTITY() AS int)
-	--return
+    SET @CartId = SCOPE_IDENTITY()
+    RETURN @CartId
 END
     GO
 
-	/* InsertIntoCart */
+DECLARE @CartIdOut int;
+EXEC @CartIdOut = CreateCart 1, 1, 1, 1
+SELECT @CartIdOut AS CartId
+GO
+
+select * from cart go
+
+/* InsertIntoCart */
 CREATE OR ALTER PROCEDURE InsertIntoCart
-	(@ProductId int, @Amount int, @Price int)
+    (@ProductId int,
+    @Amount int,
+    @Price int)
 AS
 BEGIN
     INSERT INTO Cart
@@ -75,30 +86,33 @@ GO
 
 /* UpdateCart */
 CREATE OR ALTER PROCEDURE UpdateCart
-   (@CartId int, @ProductId int, @Amount int, @Price int)
+    (@CartId int,
+    @ProductId int,
+    @Amount int,
+    @Price int)
 AS
 BEGIN
-	UPDATE Cart
+    UPDATE Cart
 	SET Cart.Amount = Cart.Amount + @Amount, Cart.Price = @Price
-	WHERE Cart.Id = @CartId 
-	AND Cart.ProductId = @ProductId
+	WHERE Cart.Id = @CartId
+        AND Cart.ProductId = @ProductId
 END
     GO
 
-	UpdateCart 3, 3, 4, 220
-	go
+UpdateCart 3, 3, 4, 220
+	GO
 
-delete   from cart
-go
+DELETE   FROM cart
+GO
 /* GetCart */
 CREATE OR ALTER PROCEDURE GetCart
-(@CartId int)
+    (@CartId int)
 AS
 BEGIN
     SELECT p.Name, c.Amount, c.Price, c.Sum
     FROM Cart c
         INNER JOIN Products p ON c.ProductId = p.Id
-		WHERE c.Id = @CartId;
+    WHERE c.Id = @CartId;
 END
     GO
 EXEC GetCart 1
